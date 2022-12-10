8109af3a248ddb794338988bc782259b69ce334e
gabc123123 <gabc123123@gmail.com>
2022-12-10 17:13:02 +0200

    update

--- data/quoteJson.js
+++ data/quoteJson.js
@@ -2,217 +2,174 @@ var quoteJson =
 [
     {
         "text": "Always remember that you are absolutely unique. Just like everyone else. Margaret Mead",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "An expert is a man who has made all the mistakes which can be made, in a narrow field. Niels Bohr",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Be not afraid of greatness. Some are born great, some achieve greatness, and others have greatness thrust upon them. William Shakespeare",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Dogs laugh, but they laugh with their tails. Max Eastman",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Don't bother just to be better than your contemporaries or predecessors. Try to be better than yourself.  William Faulkner",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Don't compare your life to others. There's no comparison between the sun and the moon. They both shine when it's their time. Unknown",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Don't judge each day by the harvest you reap but by the seeds that you plant. Robert Louis Stevenson",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Don\u2019t worry about failures; worry about the chances you miss when you don\u2019t even try. Jack Canfield",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Everyone wants to ride with you in the limo, but what you want is someone who will take the bus with you when the limo breaks down. Oprah Winfrey",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If education is always to be conceived along the same antiquated lines of a mere transmission of knowledge, there is little to be hoped from it in the bettering of man's future. Maria Montessori",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If life were predictable it would cease to be life and be without flavor. Eleanor Roosevelt",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If life were predictable it would cease to be life, and be without flavor. Eleanor Roosevelt",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If you can dream it, you can do it. Walt Disney",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. Oprah Winfrey",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. James Cameron",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "If you want extraordinary results, you must put in extraordinary efforts. Cory Booker",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "It is during our darkest moments that we must focus to see the light. Aristotle",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Its just human nature. People forget past and only care about their present and don't think what will happen in future... Taimoor Madni",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Love is a strange feeling! And you cannot understand what happens to you when it does happen to you. Avijeet Das",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Many of life's failures are people who did not realize how close they were to success when they gave up. Thomas A. Edison",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "My body is my journal, and my tattoos are my story. Johnny Depp",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Our doubts are traitors and make us lose the good we oft might win by fearing to attempt. William Shakespeare",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. Thomas A. Edison",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time. Thomas A. Edison",
-        "url": "https:\/\/www.brainyquote.com\/quotes\/thomas_a_edison_149049",
-        "tag": "#quote"
+        "url": "https:\/\/www.brainyquote.com\/quotes\/thomas_a_edison_149049"
     },
     {
         "text": "People who are crazy enough to think they can change the world are the ones who do. Rob Siltanen",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Read as much as you can to keep the mind as supple as a baby's bum. Jeff Bjune",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Strive not to be a success, but rather to be of value. Albert Einstein",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Tell me and I forget. Teach me and I remember. Involve me and I learn. Benjamin Franklin",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The best and most beautiful things in the world cannot be seen or even touched \u2014 they must be felt with the heart. Helen Keller",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The future belongs to those who believe in the beauty of their dreams. Eleanor Roosevelt",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The greatest glory in living lies not in never falling, but in rising every time we fall. Nelson Mandela",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The greatest glory in living lies not in never falling, but in rising every time we fall. Nelson Mandela",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The hard part about writing a novel is finishing it. Ernest Hemingway",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The heart and soul of a company is creativity and innovation. Robert Iger",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "The way to get started is to quit talking and begin doing. Walt Disney",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Those who dare to fail miserably can achieve greatly. John F. Kennedy",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "We can\u2019t become what we need to be by remaining what we are. Oprah Winfrey",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "We know what we are, but know not what we may be. William Shakespeare",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "When something is important enough, you do it even if the odds are not in your favor. Elon Musk",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "When something is important enough, you do it even if the odds are not in your favor. Elon Musk",
-        "url": "https:\/\/www.brainyquote.com\/quotes\/elon_musk_567219",
-        "tag": "#quote"
+        "url": "https:\/\/www.brainyquote.com\/quotes\/elon_musk_567219"
     },
     {
         "text": "When you fail you learn from the mistakes you made and it motivates you to work even harder. Natalie Gulbis",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Whoever is happy will make others happy too. Anne Frank",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     },
     {
         "text": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma \u2013 which is living with the results of other people's thinking. Steve Jobs",
-        "url": "",
-        "tag": "#quote"
+        "url": ""
     }
 ]
